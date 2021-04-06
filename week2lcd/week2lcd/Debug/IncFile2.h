4312506bda0086aee82a8431d9e77a0cf69def41
BartvanGool <60600546+BartvanGool@users.noreply.github.com>
2021-04-06 13:08:49 +0200

    Add files via upload

---
+++ week2lcd/week2lcd/Debug/IncFile2.h
@@ -0,0 +1,6 @@
+#ifndef INCFILE2_H
+#define INCFILE2_H
+
+void init();
+void display_text(char *str);
+void set_cursor(int position);#endif
---
+++ week2lcd/week2lcd/Debug/lcd.h
@@ -0,0 +1,8 @@
+#ifndef lcd_H
+#define lcd_H
+
+void init(void);
+void display_text(char *str);
+void set_cursor(int position);
+
+#endif
